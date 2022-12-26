53b891047f2bf9dabfbc8d882baa4963780bcd02
Almir Durakovic <almir.durakovic84@gmail.com>
2022-12-26 19:50:41 +0100

    Adding modifiedIII

--- cypress/e2e/pageObjects/addBoard.js
+++ cypress/e2e/pageObjects/addBoard.js
@@ -2,14 +2,11 @@ class AddBoard {
     get addNew() {
         return cy.get('.vs-c-my-organization__board-new')
     }
-    get board() {
-        return cy.get('[style="transform-origin: left center; z-index: 2145; display: none;"]')
-    }
-
+  
     get boardTitle() {
         return cy.get('[placeholder="Enter title..."]')
     }
-    get nextBtn1() {
+    get nextBtn() {
         return cy.get('[name="next_btn"]')
     }
     get boardType() {
@@ -18,7 +15,7 @@ class AddBoard {
     createBoard(name, title) {
        
         this.boardTitle.type(title)
-        this.nextBtn1.click()
+        this.nextBtn.click()
         this.boardType.click()
         this.nextBtn1.click()
         this.nextBtn1.click()
